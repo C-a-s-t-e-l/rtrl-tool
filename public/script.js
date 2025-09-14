4785225434474d9738a85199855af6fd69f8aa36
CasCodes <ezzyllena@gmail.com>
2025-09-14 23:44:32 +0800

    Update script.jsa

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // --- IMPORTANT: MAKE SURE THIS IS YOUR CURRENT, ACTIVE NGROK URL ---
-const BACKEND_URL = 'https://a4b083eaf85b.ngrok-free.app'; // <--- PASTE YOUR NEW NGROK URL HERE
+const BACKEND_URL = 'https://2e51e6002e2d.ngrok-free.app'; // <--- PASTE YOUR NEW NGROK URL HERE
 
 // This function will be called AFTER the CDN script has loaded successfully.
 function initializeMainApp() {
@@ -319,3 +319,4 @@ socketIoScript.onerror = () => {
 
 // 4. Add the script to the page to start the loading process.
 document.head.appendChild(socketIoScript);
+
