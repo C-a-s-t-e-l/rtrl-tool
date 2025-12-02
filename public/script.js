5250d8e0f6ada8ae5ce1a8e05a6906f050d519eb
Cas <ezzyllena@gmail.com>
2025-12-02 20:12:48 +0800

    poll tst

--- public/script.js
+++ public/script.js
@@ -52,7 +52,7 @@ function initializeMainApp() {
 
   const socket = io(BACKEND_URL, {
     extraHeaders: { "ngrok-skip-browser-warning": "true" },
-    transports: ["websocket"],
+    transports: ["websocket", "polling"],
     timeout: 70000,
   });
 
