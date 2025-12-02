6bb1e6380354d611e23132fb98162b3b8535c9bb
Cas <ezzyllena@gmail.com>
2025-12-02 21:48:41 +0800

    rmv poll

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 const io = new Server(server, {
   cors: { origin: FRONTEND_URL, methods: ["GET", "POST"] },
-  transports: ['websocket', 'polling'],
+  transports: ['websocket'], 
   pingInterval: 25000,
   pingTimeout: 60000,
 });
